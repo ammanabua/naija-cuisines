574a3b15b68c87c16c00316b05e578452d8b9add
Amman Abua <ammanabua@gmail.com>
2022-03-24 17:15:15 +0100

    add product page and footer component

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,7 +1,47 @@
+import styles from "../styles/Footer.module.css";
+import Image from "next/image";
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <div className={styles.container}>
+      <div className={styles.item}>
+        <Image src="/img/bg.jpg" layout="fill" alt="" objectFit="cover" />
+      </div>
+      <div className={styles.item}>
+        <div className={styles.card}>
+          <h2 className={styles.motto}>
+            OH YES, WE MAKE THE MOST DELICIOUS MEALS IN THE BUJ
+          </h2>
+        </div>
+        <div className={styles.card}>
+          <h1 className={styles.title}>OUR LOCATION</h1>
+          <p className={styles.text}>
+            No. 43 Parakou Crescent,
+            <br /> Wuse II, Abuja
+            <br /> 0818 234 2346
+          </p>
+        </div>
+        <div className={styles.card}>
+          <h1 className={styles.title}>WORKING HOURS</h1>
+          <p className={styles.text}>
+            MONDAY - FRIDAY 
+            <br /> 9:00 - 22:00
+          </p>
+          <p className={styles.text}>
+            SATURDAY - SUNDAY 
+            <br /> 12:00 - 22:00
+          </p>
+        </div>
+        <div className={styles.card}>
+          <h1 className={styles.title}>WE ACCEPT</h1>
+          <p className={styles.text}>
+            MASTERCARD 
+            <br />VISA
+            <br /> VERVE
+          </p>
+        </div>
+      </div>
+    </div>
   )
 }
 
---
+++ pages/product/[id].jsx
@@ -0,0 +1,64 @@
+import styles from "../../styles/Product.module.css"
+import Image from "next/image"
+import { useState } from "react"
+
+const Product = () => {
+
+    const [size, setSize] = useState(0)
+    const food = {
+        id: 1,
+        img: "/img/featured4.jpg",
+        name: "Chicken Fried Rice",
+        price: [2000, 3500, 5000],
+        desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque porro atque voluptates!"
+    };
+
+  return (
+    <div className={styles.container}>
+        <div className={styles.left}>
+            <div className={styles.imgContainer}>
+                <Image src={food.img} layout="fill" alt="" objectFit="contain" />
+            </div>
+        </div>
+        <div className={styles.right}>
+            <h1 className={styles.title}>{food.name}</h1>
+            <span className={styles.price}>N{food.price[size]}</span>
+            <p className={styles.desc}>{food.desc}</p>
+            <h3 className={styles.choose}>Choose your size</h3>
+            <div className={styles.sizes}>
+                <div className={styles.size} onClick={() => setSize(0)}>
+                    <Image src="/img/plate.png" layout="fill" alt="" />
+                    <span className={styles.number}>Small</span>
+                </div>
+                <div className={styles.size} onClick={() => setSize(1)}>
+                    <Image src="/img/plate.png" layout="fill" alt="" />
+                    <span className={styles.number}>Medium</span>
+                </div>
+                <div className={styles.size} onClick={() => setSize(2)}>
+                    <Image src="/img/plate.png" layout="fill" alt="" />
+                    <span className={styles.number}>Large</span>
+                </div>
+            </div>
+
+            <h3 className={styles.choose}>Extras</h3>
+            <div className={styles.extras}>
+                <div className={styles.option}>
+                    <input type="checkbox" id="spicy" name="spicy" className={styles.checkbox} />
+                    <label htmlFor="spicy">Small Pepper</label>
+                </div>
+                <div className={styles.option}>
+                    <input type="checkbox" id="extra-spicy" name="extra-spicy" className={styles.checkbox} />
+                    <label htmlFor="extra-spicy">Plenty Pepper</label>
+                </div>
+            </div>
+            <div className={styles.add}>
+                <input type="number" defaultValue={1} className={styles.quantity} />
+                <button className={styles.button}>Add to Cart</button>
+            </div>
+        </div>
+
+    </div>
+  )
+}
+
+export default Product
