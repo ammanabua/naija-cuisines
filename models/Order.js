72bc434a1ad5587f831d181f25269f1348500c9f
Amman Abua <ammanabua@gmail.com>
2022-04-04 13:10:58 +0100

    create order model

---
+++ models/Order.js
@@ -0,0 +1,42 @@
+import mongoose from "mongoose";
+
+
+const OrderSchema = new mongoose.Schema({
+    customer: {
+        type: String,
+        required: true,
+        maxlength: 60,
+    },
+
+    phone : {
+        type: Number,
+        required: true,
+    },
+
+    address: {
+        type: String,
+        required: true,
+        maxlength: 200,
+    },
+
+    total: {
+        type: Number,
+        required: true,
+    },
+
+    status: {
+        type:Number,
+        default: 0,
+        required: true
+    },
+
+    method: {
+        type:Number,
+        required: true
+    },
+
+}, {timestamps: true}
+);
+
+
+export default mongoose.models.Order || mongoose.model("Order", OrderSchema);
