d9d0de7d013c420036de2c1a2258a2cebfa8db90
Amman Abua <ammanabua@gmail.com>
2023-01-04 07:26:53 +0100

    update Services compnent

--- components/About.jsx
+++ components/About.jsx
@@ -1,4 +1,5 @@
-import Image from 'next/image'
+import Image from 'next/image';
+import Link from 'next/link';
 import React from 'react'
 import styles from "../styles/About.module.css"
 
@@ -7,12 +8,14 @@ const About = () => {
     <section className={styles.container}>
         <div className={styles.wrapper}>
             <div className={styles.left}>
-                <Image src="/img/featured1.jpg" alt="" layout='fill' objectFit="contain" />
+                <Image src="/img/abt.jpg" alt="" width="800" height="800" objectFit="fill" />
             </div>
             <div className={styles.right}>
-                <article className={styles.header}>
-                    About Taval Cuisinier
-
+                <article className={styles.article}>
+                    <div className={styles.articleHead}>
+                    About Us
+                    </div>
+                    <div className={styles.articleText}>
                     <p className={styles.text} >
                         At Taval Cuisiniere, we are dedicted to presenting exceptional food, regardless of where it implies going the additional mile.
                         <br />
@@ -20,6 +23,11 @@ const About = () => {
                         An indigenous and professional catering service that started, Taval Cuisinier offers the best food, curates exceptional events and deliver your meals with a personal. 
                         <br />
                     </p>
+
+                    <Link href="/" passHref>
+                        <button className={styles.btn}>Learn more...</button>
+                    </Link>
+                    </div>
                 </article>
             </div>
         </div>
